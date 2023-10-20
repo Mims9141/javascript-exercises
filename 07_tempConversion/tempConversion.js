fc9614d9426a216e5d0a0809373ae33e4a17617e
muaz <muazibrahim.95@outlook.com>
2023-10-20 17:43:24 -0500

    q6 and q7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * 5 / 9;
+  if(Number.isInteger(tempC)){
+    return tempC;
+  } else{
+    tempC = Math.round(tempC * 1e1) / 1e1
+    return tempC
+  };
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC * 9 / 5) + 32;
+  if(Number.isInteger(tempF) == true){
+    return tempF;
+  } else{
+    tempF = Math.round(tempF * 1e1) / 1e1;
+    return tempF;
+  };
 };
 
 // Do not edit below this line
